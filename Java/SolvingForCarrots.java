edf7444f905c8bef0dc1fba8d742b84dc8b7c3f5
Bivash Pandey <36802916+bivashpandey@users.noreply.github.com>
2020-11-16 15:57:44 -0400

    Solving for Carrots

---
+++ Java/SolvingForCarrots.java
@@ -0,0 +1,20 @@
+/*
+* Problem: https://open.kattis.com/problems/carrots
+* 
+* @author  Bivash Pandey
+*/
+
+import java.util.Scanner;
+
+public class SolvingForCarrots {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int p = input.nextInt();
+        input.nextLine();
+        for(int i=1;i<=n;i++) {
+            String str = input.nextLine();
+        }
+        System.out.println(p);
+    }
+}
