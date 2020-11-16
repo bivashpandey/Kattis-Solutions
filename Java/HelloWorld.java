1ab18ab8fabfb9ecbc3fb648ed845cfe9896f52b
Bivash Pandey <36802916+bivashpandey@users.noreply.github.com>
2020-11-16 15:46:18 -0400

    Hello World!

---
+++ Java/HelloWorld.java
@@ -0,0 +1,11 @@
+/*
+* Problem: https://open.kattis.com/problems/hello
+* 
+* @author  Bivash Pandey
+*/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
