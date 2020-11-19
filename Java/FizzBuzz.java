b74790a76d24dcabb4fff6d210e0c37b84269636
Bivash Pandey <36802916+bivashpandey@users.noreply.github.com>
2020-11-19 19:12:59 -0400

    FizzBuzz

---
+++ Java/FizzBuzz.java
@@ -0,0 +1,30 @@
+/*
+* Problem: https://open.kattis.com/problems/fizzbuzz
+* 
+* @author  Bivash Pandey
+*/
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int n = input.nextInt();
+        for(int i = 1; i<=n; i++) {
+            if(i%x == 0 && i%y == 0) {
+                System.out.println("FizzBuzz");
+            }
+            else if(i%x == 0) {
+                System.out.println("Fizz");
+            }
+            else if(i%y == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
+        }
+    }
+}
